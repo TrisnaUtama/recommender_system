92575d4f4b4614cca2eb4aacd4690860deb6d591
Trisna Utama <trisnautama@gmail.com>
2025-06-26 01:51:24 +0700

    init

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 DB_CONFIG = {
-    'host': 'db',
-    'port': 5432,
-    'dbname': 'rental',
-    'user': 'rental',
-    'password': 'rental'
+    'host': 'db_testing',
+    'port': 5433,
+    'dbname': 'rental_testing',
+    'user': 'rental_testing',
+    'password': 'rental_testing_password'
 }
 
 MODEL_PATH = "./base-model/user_cf_model.pkl"
